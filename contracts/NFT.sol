d3714bc3a1cad66c4398707bf3915b50e2d03618
Asim <asimraza243@gmail.com>
2021-07-13 14:21:04 +0500

    contract updated

---
+++ contracts/NFT.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.3;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    address contractAddress;
+
+    constructor(address _contractAddress) ERC721("Cubix Tokens", "CT") {
+        contractAddress = _contractAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    }
+}
---
+++ test/sample-test.js
@@ -0,0 +1,47 @@
+// const { expect } = require("chai");
+
+describe("NFTMarket", function () {
+  it("Should return and execute market sales", async function () {
+    const Market = await ethers.getContractFactory("NFTMarket");
+    const market = await Market.deploy();
+    await market.deployed();
+    const marketAddress = market.address;
+    console.log("NFT Market deployed at:",marketAddress);
+    const NFT = await ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy(marketAddress);
+    await nft.deployed();
+    const nftContractAddress = nft.address;
+    console.log("NFT Contract deployed at:",nftContractAddress);
+
+    let listingPrice = await market.getListingPrice();
+    listingPrice = listingPrice.toString();
+    // console.log(listingPrice);
+    const auctionPrice = ethers.utils.parseUnits('1','ether');
+    // console.log(auctionPrice);
+    await nft.createToken("https://www.google.com.pk");
+    await nft.createToken("https://www.facebook.com");
+
+  const item1 =  await market.createMarketItem(nftContractAddress,1,auctionPrice,{value: listingPrice})
+  const item2 =  await market.createMarketItem(nftContractAddress,2,auctionPrice,{value: listingPrice})
+  // console.log(item);
+
+  const [_,buyerAddress] = await ethers.getSigners();
+  await market.connect(buyerAddress).createMarketSale(nftContractAddress,2,{value:auctionPrice})
+
+  items = await market.fetchMarketItem();
+  items = await Promise.all(items.map(async i => {
+  const tokenUri = await nft.tokenURI(i.tokenId);
+    console.log(i);
+    let item = {
+      price: i.price.toString(),
+      tokenId: i.tokenId.toString(),
+      owner: i.owner,
+      seller:i.seller,
+      tokenUri
+    }
+
+    return item;
+  }))
+  console.log("Items: ",items);
+  });
+});
